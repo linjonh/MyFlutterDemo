2f549c1acb206d0c26b222cef9c8114a79a6fc82
linjy <linjianyou@live.cn>
2024-11-15 11:10:45 +0800

    增加splashScreen API，展示第一屏闪屏启动页

--- android/app/src/main/kotlin/com/jaysen/demo/my_flutter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/jaysen/demo/my_flutter_demo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.jaysen.demo.my_flutter_demo
 
+import android.os.Build
+import android.os.Bundle
 import android.util.Log
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -11,6 +14,24 @@ class MainActivity : FlutterActivity() {
     }
 
     private val CHANNEL = "com.jaysen/test"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            // Disable the Android splash screen fade out animation to avoid
+//            // a flicker before the similar frame is drawn in Flutter.
+//            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+//        }
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            Log.d(
+                TAG,
+                "onCreate: setOnExitAnimationListener: splashScreenViewProvider=${splashScreenViewProvider.iconAnimationDurationMillis}"
+            )
+            splashScreenViewProvider.remove()
+        }
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
--- lib/dart_lang_test/function_param/function_example.dart
+++ lib/dart_lang_test/function_param/function_example.dart
@@ -8,12 +8,11 @@ void testFunOptionParam(String name, String two, [String? six, int? seven]) {
 }
 
 ///命名参数
-void testFunNamedParam(String name, String two,
-    {required String three, String? four, String five = "default five"}) {
+void testFunNamedParam(String name, String two, {required String three, String? four, String five = "default five"}) {
   print("$name $two $three $four $five");
 }
 
 void main() {
   testFunNamedParam("named", "two", three: "three");
-  testFunOptionParam("option", "two","six");
+  testFunOptionParam("option", "two", "six");
 }
